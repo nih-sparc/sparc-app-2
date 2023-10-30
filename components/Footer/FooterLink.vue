d1def4dcabbf3b1b0b49501ab0497164cb640f39
Eric Gauzens <ericgauzens@hotmail.com>
2023-10-30 14:54:45 -0400

    Footer being displayed

---
+++ components/Footer/FooterLink.vue
@@ -0,0 +1,49 @@
+<template>
+  <a
+    :href="linkUrl"
+    :target="!opensInNewTab(linkUrl) && !isFeedbackLink ? '_self' : '_blank'"
+  >
+  {{ link.fields.title }}
+  </a>
+</template>
+
+<script>
+import { opensInNewTab } from '@/mixins/marked/index'
+
+export default {
+  name: 'FooterLink',
+
+  props: {
+    link: {
+      type: Object,
+      default: () => {
+        return {
+          sys: {},
+          fields: {}
+        }
+      }
+    }
+  },
+
+  methods: {
+    opensInNewTab
+  },
+
+  computed: {
+    currentUrl() {
+      return encodeURIComponent(this.$route.fullPath)
+    },
+    isFeedbackLink() {
+      const title = this.link.fields.title
+      return title == 'Site Feedback'
+    },
+    linkUrl() {
+      let url = this.link.fields.longUrl || this.link.fields.url
+      if (this.isFeedbackLink) {
+        url = url + `&source_url=${this.currentUrl}`
+      }
+      return url
+    }
+  }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,126 @@
+<template>
+  <div :class="[scrollingState ? 'layout' : '']">
+    <!--<sparc-header />-->
+    <slot />
+    <sparc-footer />
+    <!--<cookie-notice v-if="!hasAcceptedGDPR" />-->
+  </div>
+</template>
+
+<script>
+//import sparcHeader from '@/components/header/Header.vue'
+import sparcFooter from '@/components/Footer/Footer.vue'
+//import CookieNotice from '@/components/CookieNotice/CookieNotice.vue'
+import { pathOr, propOr } from 'ramda'
+import DOMPurify from 'isomorphic-dompurify'
+import { useMainStore } from '../store/index.js'
+
+export default {
+  components: {
+    //CookieNotice,
+    //sparcHeader,
+    sparcFooter
+  },
+  data() {
+    return {
+      store: useMainStore()
+    }
+  },
+  computed: {
+    scrollingState() {
+      return pathOr('', ['state','disableScrolling'], this.store)
+    },
+    hasAcceptedGDPR() {
+      return pathOr('', ['state','hasAcceptedGDPR'], this.store)
+    },
+    hasSeenPortalNotification() {
+      return pathOr('', ['state','hasSeenPortalNotification'], this.store)
+    },
+    portalNotification() {
+      return pathOr('', ['state','portalNotification'], this.store)
+    },
+  },
+  mounted() {
+    this.showPortalNotification()
+  },
+  head() {
+    return {
+      meta: [
+        { 
+          hid: 'og:url',
+          property: 'og:url',
+          content: `${process.env.ROOT_URL}${this.$route.fullPath}`,
+        },
+      ]
+    }
+  },
+  methods: {
+    showPortalNotification() {
+      const displayOnHomePageOnly = propOr("", 'displayOn', this.portalNotification) === 'Homepage Only'
+      const currentlyOnHomePage = this.$route.fullPath === "/"
+      const message = DOMPurify.sanitize(propOr("", 'message', this.portalNotification).trim(), {
+        ALLOWED_TAGS: ['a', 'br', 'sup'], // We allow links, line breaks, and sup tags
+        ALLOWED_ATTR: ['href'], // We allow the href attribute for links
+      })
+      const messageType = propOr("", 'messageType', this.portalNotification)
+      const onlyShowOnce = propOr(true, 'showOnce', this.portalNotification)
+      const stopShowingDate = propOr(undefined, 'stopShowingDate', this.portalNotification)
+      // If the stop showing time is not set then always display message, therwise check if the date has passed
+      const stopShowing = stopShowingDate === undefined ? false : new Date(stopShowingDate).getTime() < new Date().getTime()
+      if (message != "" && !stopShowing) {
+        if (!onlyShowOnce || !this.hasSeenPortalNotification) {
+          if (!displayOnHomePageOnly || (displayOnHomePageOnly && currentlyOnHomePage)) {
+            switch (messageType) {
+              case 'Error': {
+                this.$message({
+                  message: message,
+                  showClose: true,
+                  iconClass: 'el-icon-circle-close',
+                  customClass: 'el-message--error',
+                  dangerouslyUseHTMLString: true,
+                  duration: 0
+                })
+                break
+              }
+              case 'Success': {
+                this.$message({
+                  message: message,
+                  showClose: true,
+                  iconClass: 'el-icon-circle-check',
+                  customClass: 'el-message--success',
+                  dangerouslyUseHTMLString: true,
+                  duration: 0
+                })
+                break
+              }
+              case 'Information': {
+                this.$message({
+                  message: message,
+                  showClose: true,
+                  iconClass: 'about-icon',
+                  customClass: 'el-message--info',
+                  dangerouslyUseHTMLString: true,
+                  duration: 0
+                })
+                break
+              }
+            }
+            /*const today = new Date()
+            const expirationDate = new Date(today.setDate(today.getDate() + 30))
+            this.$cookies.set('PortalNotification:hasBeenSeen', true, { expires: expirationDate })*/
+          }
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.layout {
+  @media screen and (max-width: 768px) {
+    overflow: hidden;
+    position: fixed;
+  }
+}
+</style>
