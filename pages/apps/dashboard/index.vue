298d2f287dee1513452d983e14115ed6cd9877a2
Samantha Kraft <kraftsam@gmail.com>
2024-04-29 16:30:27 -0400

    add dashboard to sparc pages, add script to plugins for mitt

---
+++ pages/apps/dashboard/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <SparcDashboard></SparcDashboard>
+</template>
+<script setup lang="ts">
+import SparcDashboard from "@/TestDashboard/src/components/SparcDashboard.vue"
+import '@/plugins/dashboard.js'
+
+</script>
+<style scoped lang="scss">
+
+
+
+</style>
---
+++ plugins/dashboard.js
@@ -0,0 +1,25 @@
+import mitt from 'mitt'
+import { defineAsyncComponent } from 'vue'
+import { useGlobalVarsStore } from '@/TestDashboard/src/stores/globalVars'
+import "@/TestDashboard/tailwind/output.css"
+
+const emitter = mitt();
+
+const componentMap = [
+    'ImageSelector',
+    'LocationNav',
+    'FlatmapViewer',
+    'BiolucidaViewer',
+    'QDBChart'
+]
+
+export default defineNuxtPlugin((nuxtApp) => {
+        console.log("emmiter")
+        nuxtApp.vueApp.provide('emitter', emitter);
+        componentMap.forEach(comp=>{
+            const asyncComponent = defineAsyncComponent(() => import(`@/TestDashboard/src/components/${comp}.vue`)); 
+            nuxtApp.vueApp.component(comp, asyncComponent);
+        })
+        const globalVars = useGlobalVarsStore();
+        globalVars.componentList = componentMap;
+    })
