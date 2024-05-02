8e2713f85448931fdbe14cf0423d702b650485ba
Eric Gauzens <ericgauzens@hotmail.com>
2024-05-02 17:19:03 -0400

    Added dashboard

---
+++ pages/apps/dashboard/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <SparcDashboard />
+</template>
+<script setup>
+  import SparcDashboard from "@/TestDashboard/src/components/SparcDashboard.vue"
+</script>
+<style scoped lang="scss"></style>
---
+++ plugins/dashboard.js
@@ -0,0 +1,23 @@
+import mitt from 'mitt'
+import { useGlobalVarsStore } from '@/TestDashboard/src/stores/globalVars'
+import "@/TestDashboard/tailwind/output.css"
+
+const emitter = mitt();
+
+const componentMap = [
+    'ImageSelector',
+    'LocationNav',
+    'FlatmapViewer',
+    'BiolucidaViewer',
+    'QDBChart'
+]
+
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.vueApp.provide('emitter', emitter);
+    componentMap.forEach(comp=>{
+        const asyncComponent = defineAsyncComponent(() => import(`@/TestDashboard/src/components/${comp}.vue`)); 
+        nuxtApp.vueApp.component(comp, asyncComponent);
+    })
+    const globalVars = useGlobalVarsStore();
+    globalVars.componentList = componentMap;
+})
