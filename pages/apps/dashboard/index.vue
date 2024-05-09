50540673662f8afb1e05bc3b7d3ea24690046832
Eric Gauzens <ericgauzens@hotmail.com>
2024-05-09 15:05:19 -0400

    Replace submodule with module

--- pages/apps/dashboard/index.vue
+++ pages/apps/dashboard/index.vue
@@ -1,12 +1,8 @@
 <template>
-    <SparcDashboard />
+  <SparcDashboard />
 </template>
 <script>
-import SparcDashboard from "@/TestDashboard/src/components/SparcDashboard.vue"
 export default {
   name: 'SparcDashboardPage',
-  components: {
-    SparcDashboard
-  }
 }
 </script>
