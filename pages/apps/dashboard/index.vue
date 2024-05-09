f7594deccb9b092bca10f90440ee7f43b67fc637
Eric Gauzens <ericgauzens@hotmail.com>
2024-05-09 15:42:04 -0400

    Added breadcrumb to dashboard

--- pages/apps/dashboard/index.vue
+++ pages/apps/dashboard/index.vue
@@ -1,8 +1,30 @@
 <template>
-  <SparcDashboard />
+  <div>
+    <breadcrumb :breadcrumb="breadcrumb" :title=title />
+    <SparcDashboard />
+  </div>
 </template>
 <script>
 export default {
   name: 'SparcDashboardPage',
+  data() {
+    return {
+      title: 'SPARC Dashboard',
+      breadcrumb: [
+        {
+          label: 'Home',
+          to: {
+            name: 'index'
+          }
+        },
+        {
+          to: {
+            name: 'apps',
+          },
+          label: 'SPARC Apps',
+        }
+      ]
+    }
+  }
 }
 </script>
