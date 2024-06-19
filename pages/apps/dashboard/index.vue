44336f9517b586804a87039782ac3ef75f4b012f
Eric Gauzens <ericgauzens@hotmail.com>
2024-06-19 17:37:27 -0400

    Remove dashboard page

--- pages/apps/dashboard/index.vue
+++ pages/apps/dashboard/index.vue
@@ -1,32 +1,9 @@
-<template>
-  <div>
-    <breadcrumb :breadcrumb="breadcrumb" :title=title />
-    <client-only>
-      <SparcDashboard />
-    </client-only>
-  </div>
-</template>
 <script>
 export default {
   name: 'SparcDashboardPage',
-  data() {
-    return {
-      title: 'SPARC Dashboard',
-      breadcrumb: [
-        {
-          label: 'Home',
-          to: {
-            name: 'index'
-          }
-        },
-        {
-          to: {
-            name: 'apps',
-          },
-          label: 'SPARC Apps',
-        }
-      ]
-    }
+  setup() {
+    const router = useRouter()
+    router.push("/")
   }
 }
 </script>
