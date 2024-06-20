773042c10c16246fc5aa194ef4e2fb80fe2ad1a0
Eric Gauzens <ericgauzens@hotmail.com>
2024-06-20 11:59:09 -0400

    Add back in dashboard component

--- pages/apps/dashboard/index.vue
+++ pages/apps/dashboard/index.vue
@@ -1,9 +1,33 @@
+<template>
+  <div>
+    <breadcrumb :breadcrumb="breadcrumb" :title=title />
+    <client-only>
+      <SparcDashboard />
+    </client-only>
+  </div>
+</template>
 <script>
 export default {
   name: 'SparcDashboardPage',
-  setup() {
-    const router = useRouter()
-    router.push("/")
+  data() {
+    return {
+      title: 'SPARC Dashboard',
+      breadcrumb: [
+        {
+          label: 'Home',
+          to: {
+            name: 'index'
+          }
+        },
+        {
+          to: {
+            name: 'apps',
+          },
+          label: 'SPARC Apps',
+        }
+      ]
+    }
   }
 }
 </script>
+
