e817cff71f45a79768f4540ef89982ab0546eb38
Eric Gauzens <ericgauzens@hotmail.com>
2024-07-14 09:31:41 -0400

    updated middleware

--- server/middleware/webCrawlers.js
+++ server/middleware/webCrawlers.js
@@ -4,7 +4,7 @@ export default defineEventHandler((event) => {
   const route = event?.node?.url
   const userAgent = req?.headers['user-agent']?.toLowerCase()
   const algoliaSearchPaths = ['data', 'datasets']
-  const botNames = ['googlebot', 'semrush', 'msnbot', 'yandex', 'applebot', 'wowrack', 'lifeera', 'PetalBot', 'nettle', 'xforce-security',
+  const botNames = ['semrush', 'msnbot', 'yandex', 'applebot', 'wowrack', 'lifeera', 'PetalBot', 'nettle', 'xforce-security',
     'Neevabot', 'Seekport\sCrawler', 'Exabot', 'Gigabot', 'ICCrawler', 'Snappy', 'Mb2345Browser', 'QQBrowser', 'LieBaoFast',
     'MicroMessenger', 'Kinza', 'TheWorld', 'YoudaoBot', 'Qwantify', 'Bleriot', 'WikiApiary', 'MegaIndex', 'MojeekBot', 'BLEXBot',
     'coccocbot', 'SEOkicks', 'SeznamBot', 'YandexImages', 'TweetmemeBot', 'Yeti', 'AhrefsBot', 'Bytespider', 'MJ12bot', 'TurnitinBot',
@@ -18,11 +18,15 @@ export default defineEventHandler((event) => {
     // Grab the first directory
     firstPartOfRoute = firstPartOfRoute.substring(0, index);
   }
-
-  if (firstPartOfRoute != '' && algoliaSearchPaths.includes(firstPartOfRoute)) {
-    if (userAgent && botNames.some(botName => userAgent.indexOf(botName) !== -1)) {
+  // block all un-wanted bots and only return a 200 for googlebot so that client does not get rendered
+  if (firstPartOfRoute == '' || algoliaSearchPaths.includes(firstPartOfRoute)) {
+    if (userAgent && botNames.some(botName => userAgent.indexOf(botName.toLowerCase()) !== -1)) {
       res.statusCode = 403
       res.end('Bot detected, serving 403 response.')
+    } else if (userAgent && (userAgent.includes('googlebot') || userAgent.includes("google.com/bot.html"))) {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/plain')
+      res.end('Googlebot detected, serving empty response.')
     }
   }
 })
