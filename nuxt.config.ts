2b798a630b522c6d81817feb616cf7c9dea6bbc6
Eric Gauzens <ericgauzens@hotmail.com>
2024-09-06 06:27:39 -0400

    updated contentful portal notification entry id

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -116,7 +116,7 @@ export default defineNuxtConfig({
       CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
       CTF_API_HOST: process.env.CTF_API_HOST || 'preview.contentful.com',
       ctf_home_page_id: '4qJ9WUWXg09FAUvCnbGxBY',
-      ctf_portal_notification_entry_id: 'XiVlrkTXeKxTyN1Q2oY2Q',
+      ctf_portal_notification_entry_id: '5S8eazBlD1Y47pTO1EQfQ3',
       ctf_contact_us_form_options_id: '79rwRA0rUqUj6rc913BFsz',
       ctf_project_id: 'sparcAward',
       ctf_about_page_id: '4VOSvJtgtFv1PS2lklMcnS',
