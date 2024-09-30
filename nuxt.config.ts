c4d1788a19ae0ed91e8ada631d8e8a77b306cae4
Eric Gauzens <ericgauzens@hotmail.com>
2024-09-30 17:12:45 -0400

    add datasets wildcard to robots disallow list

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -226,6 +226,7 @@ export default defineNuxtConfig({
     // provide simple disallow rules for all robots `user-agent: *`
     // disallowing certain pages that are either redirects, authticated routes, or causing bots to recursively crawl
     disallow: [
+      '/datasets/*?*',
       '/welcome', 
       '/user', 
       '/contact-us', 
