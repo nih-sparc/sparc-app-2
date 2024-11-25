ee4e17c2a20c75ff3d8350abd64811244ad9c533
alan-wu <alan.wu@auckland.ac.nz>
2024-11-25 15:26:59 +1300

    Update sql request body sent to flatmap server.

--- services/flatmaps.js
+++ services/flatmaps.js
@@ -6,40 +6,28 @@ const speciesMap = {
   'NCBITaxon:9685': 'cat'
 }
 
-const buildConnectivitySqlStatement = function (keastIds) {
-  let sql = 'select knowledge from knowledge where entity in ('
-  if (keastIds.length === 1) {
-    sql += `'${keastIds[0]}')`
-  } else if (keastIds.length > 1) {
-    for (let i in keastIds) {
-      sql += `'${keastIds[i]}'${i >= keastIds.length - 1 ? ')' : ','} `
-    }
-  }
-  return sql
-}
-
-const buildAnatomySqlStatement = function(taxo, anatomy) {
+const buildAnatomySqlStatement = function(anatomy) {
   let anatomyString = ''
   if (anatomy.length === 1) {
-    anatomyString = `('${anatomy[0]}')`
+    anatomyString = '(?)'
   } else if (anatomy.length > 1) {
     anatomyString = '('
     for (let i in anatomy) {
-      anatomyString += `'${anatomy[i]}'${i >= anatomy.length - 1 ? ')' : ','} `
+      anatomyString += `?${i >= anatomy.length - 1 ? ')' : ','} `
     }
   }
-  let sql = `select id, entity from (select id, models from flatmaps group by models having max(created)) left join flatmap_entities on id = flatmap where models = '${taxo}' and entity in ${anatomyString}`
+  let sql = `select id, entity from (select id, models from flatmaps group by models having max(created)) left join flatmap_entities on id = flatmap where models = ? and entity in ${anatomyString}`
   return sql
 }
 
 const anatomyQuery = async (taxo, anatomy) => {
   const { $flatmapApiClient } = useNuxtApp()
-  const sqlData = { sql: buildAnatomySqlStatement(taxo, anatomy) }
+  const params = [taxo, ...anatomy]
+  const sqlData = { sql: buildAnatomySqlStatement(anatomy), params }
   return $flatmapApiClient.post('knowledge/query/', sqlData)
 }
 
 export default {
   anatomyQuery,
-  buildConnectivitySqlStatement,
   speciesMap
 }
