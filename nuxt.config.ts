23924bf1d5eed332035f6263440693b4fd1b327b
Eric Gauzens <ericgauzens@hotmail.com>
2025-01-23 15:18:54 -0500

    added /data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -233,6 +233,7 @@ export default defineNuxtConfig({
     // disallowing certain pages that are either redirects, authticated routes, or causing bots to recursively crawl
     disallow: process.env.DEPLOY_ENV === 'production' ? 
     [
+      '/data',
       '/datasets/file',
       '/datasets/*/version/',
       '/file',
