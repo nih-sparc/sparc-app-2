cf3d85327efae1538fa4d6a48a30159d3708eea0
alan-wu <alan.wu@auckland.ac.nz>
2025-07-08 16:58:34 +1200

    Support View Scaffold button for all datasets with scaffold.

--- components/DatasetDetails/DatasetActionBox.vue
+++ components/DatasetDetails/DatasetActionBox.vue
@@ -7,7 +7,7 @@
       </sparc-pill>
     </div>
     <div class="button-container">
-      <template v-if="datasetTypeName === 'scaffold' && !datasetInfo.study">
+      <template v-if="canViewScaffold">
         <template v-if="hasFiles">
           <el-button
             class="dataset-button"
@@ -22,7 +22,7 @@
             Get Scaffold
           </el-button>
         </template>
-        <el-button class="secondary" @click="actionButtonClicked('cite')">
+        <el-button v-if="datasetTypeName === 'scaffold'" class="secondary" @click="actionButtonClicked('cite')">
           Cite Scaffold
         </el-button>
       </template>
@@ -122,6 +122,12 @@ export default {
      */
     datasetImage: function() {
       return propOr('', 'banner', this.datasetInfo)
+    },
+    /**
+     * Returns whether a scaffold can be viewed
+     */
+    canViewScaffold: function() {
+      return this.datasetInfo.sciCrunch ? this.datasetInfo.sciCrunch['abi-scaffold-metadata-file'] : false
     },
     /**
      * Returns whether a simulation can be viewed
