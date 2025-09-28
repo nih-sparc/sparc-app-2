69466db9f333ecca5992e9165b4c8424a9cbd97e
Eric Gauzens <ericgauzens@hotmail.com>
2025-09-28 19:38:02 -0400

    our research button hotfix

--- pages/about/consortia/[id].vue
+++ pages/about/consortia/[id].vue
@@ -118,8 +118,7 @@ const whoWeAreButtonLink = computed(() => pathOr('', ['fields', 'whoWeAreButtonL
 const ourResearch = computed(() => pathOr('', ['fields', 'ourResearch'], consortiaItem.value))
 const ourResearchButtonText = computed(() => pathOr('', ['fields', 'ourResearchButtonText'], consortiaItem.value))
 const ourResearchButtonLink = computed(() => pathOr('', ['fields', 'ourResearchButtonLink'], consortiaItem.value))
-//Temporarily disable RE-JOIN dataset button until RE-JOIN datasets are available
-const displayOurResearchButton = computed(() => !ourResearchButtonLink.value.includes('selectedFacetIds=RE-JOIN'))
+const displayOurResearchButton = computed(() => !ourResearchButtonLink.value == '')
 const learnMore = computed(() => pathOr([], ['fields', 'learnMore'], consortiaItem.value))
 const logoUrl = computed(() => pathOr('', ['fields', 'logo', 'fields', 'file', 'url'], consortiaItem.value))
 const forInvestigators = computed(() => pathOr('', ['fields', 'forInvestigators'], consortiaItem.value))
