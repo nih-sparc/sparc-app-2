54e63e99f1c1a1aee1e6939d37841c861e01c80d
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-10-30 16:38:36 +1300

    Fix error message for species from url not found

--- pages/apps/maps/index.vue
+++ pages/apps/maps/index.vue
@@ -166,10 +166,11 @@ const checkSpecies = (route, organ, organ_name, taxo, for_species) => {
     successMessage = "A flatmap's unique id is provided, a legacy map may be displayed instead."
   } else {
     if (!target) {
-      if (organ) {
-        failMessage += `The ${organ_name} of a human male has been shown instead.`
+      failMessage = `Sorry! A flatmap for the specified species does not yet exist.`
+      if (organ && organ_name) {
+        failMessage += ` The ${organ_name} of a human male has been shown instead.`
       } else {
-        failMessage += 'A generic human male flatmap has been shown instead.'
+        failMessage += ` A generic human male flatmap has been shown instead.`
       }
     }
   }
