4b60567d4e848536987635746959753e8aa2c727
alan-wu <alan.wu@auckland.ac.nz>
2026-09-03 15:01:42 +1200

    Fix an issue with testing.

--- tests/cypress/e2e/datasets/detailtabs.cy.js
+++ tests/cypress/e2e/datasets/detailtabs.cy.js
@@ -137,7 +137,7 @@ datasetIds.forEach((datasetId) => {
                       cy.wrap($link).invoke('attr', 'href').then((href) => {
                         cy.request({ url: href, failOnStatusCode: false }).then((resp) => {
                           expect(resp.redirects, 'Redirect should exist').to.have.length.greaterThan(0)
-                          expect(resp.body.includes("challenges.cloudflare.com"), 'Redirect should exist').to.have.length.greaterThan(0)
+                          expect(resp.body.includes("challenges.cloudflare.com"), 'Redirect should exist').to.be.true
                         })
                       })
                     })
